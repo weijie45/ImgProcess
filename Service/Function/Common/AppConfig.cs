c25778bbf90b352a49ad952830146f761f580307
walter <Walter@u-gee.com>
2020-04-01 16:47:25 +0800

    新增Appconfig

--- Service/Function/Common/AppConfig.cs
+++ Service/Function/Common/AppConfig.cs
@@ -1,12 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Service.Function.Common
 {
-    class AppConfig
+    public class AppConfig
     {
+        public static string ThumbnailPixel;
+        private static Dictionary<string, string> ConnDict = ConfigurationManager.ConnectionStrings.Cast<ConnectionStringSettings>().ToDictionary(x => x.Name, x => x.ConnectionString);
+        //private static SettingsContext _settingsContext = null;
+
+        // Constructors
+        public AppConfig()
+        {
+           // _settingsContext = SettingsContext;
+        }
+
+
+        // Properties
+        //public SettingsContext SettingsContext
+        //{
+        //    get
+        //    {
+        //        // Creat
+        //        if (_settingsContext == null) {
+        //            // Resolve
+        //            _settingsContext = new Settings.SettingsContextModule().Create("Test");
+        //            if (_settingsContext == null) throw new InvalidOperationException("_settingsContext=null");
+        //        }
+
+        //        // Return
+        //        return _settingsContext;
+        //    }
+        //}
+
+        //public static void Init()
+        //{
+
+        //    ThumbnailPixel = _settingsContext.GetValue("ThumbnailPixel");
+        //}
     }
 }
